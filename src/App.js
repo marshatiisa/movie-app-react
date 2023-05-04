5e4a12b2c51085a9afc348744225a492de796a8b
Marsha <marshatiisa35@gmail.com>
2023-05-03 22:21:00 -0400

    remove favs component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieList from './components/MovieList';
 import SearchBox from './components/SearchBox';
 import MovieListHeading from './components/MovieListHeading';
 import AddFavorites from './components/AddToFavorites';
+import RemoveFavorites from './components/RemoveFavorites';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
@@ -12,8 +13,9 @@ const App = () => {
   	const [favorites, setFavorites] = useState([]);
 
   const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?i=tt3896198&s=${searchValue}&apikey=83e2c603`
-		// const url = `http://www.omdbapi.com/?i=tt3896198&apikey=83e2c603`;
+	const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=83e2c603`
+
+    //const url = `http://www.omdbapi.com/?i=tt3896198&s=${searchValue}&apikey=83e2c603`
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -25,6 +27,13 @@ const App = () => {
 
 	const addFavoriteMovie = (movie) => {
 		const newFavoriteList = [...favorites, movie];
+		setFavorites(newFavoriteList);
+	};
+
+	const removeFavoriteMovie = (movie) => {
+		const newFavoriteList = favorites.filter(
+			(favorite) => favorite.imdbID !== movie.imdbID
+		);
 		setFavorites(newFavoriteList);
 	};
 
@@ -44,8 +53,18 @@ const App = () => {
 			<div className='row'>
 				<MovieList 
 					movies={movies} 
-					favouriteComponent={AddFavorites}
+					favoriteComponent={AddFavorites}
 					handleFavoritesClick={addFavoriteMovie}
+				 /> 
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favorites' />
+			</div>
+			<div className='row'>
+				<MovieList 
+					movies={favorites} 
+					favoriteComponent={RemoveFavorites}
+					handleFavoritesClick={removeFavoriteMovie}
 				 /> 
 			</div>
 		</div>
